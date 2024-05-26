b01d874158c8664e913dd754c2ecffab4e270877
Auvitly <nosfosters@mail.ru>
2024-05-27 01:04:04 +0300

    UPD

--- examples/relax/main.go
+++ examples/relax/main.go
@@ -58,6 +58,8 @@ func onTime(ctx context.Context, _ any) error {
 func method(ctx context.Context) (err *stderrs.Error) {
 	defer recovery.On(&err).Do(ctx)
 
+	err = stderrs.Unknown.SetMessage("Successfully assigned the error! Wow!")
+
 	panic("I'm dropping the app now! Be afraid!")
 
 	return nil
@@ -87,13 +89,13 @@ func main() {
 /* OUT:
 > [ INFO ]  I managed to get to the conclusion
 > [ WARN ]  I'm dropping the app now! Be afraid!
-> [ ERROR ] Panic did not overtake us! We received an error: {
+> [ ERROR ] Panic did not overtake us! We received an error: We received an error: {
 	"code": "panic",
 	"message": "internal server error: unhandled exception",
 	"fields": {
 		"panic":"I'm dropping the app now! Be afraid!"
 	},
-	"embed": [
+	"embed":[
 		file already closed
 		{
 			"code": "internal",
@@ -106,19 +108,24 @@ func main() {
 				"stack":"goroutine 8 [running]:
 					runtime/debug.Stack()
 						C:/Program Files/Go/src/runtime/debug/stack.go:24 +0x5e
-							github.com/auvitly/go-tools/recovery.Builder.use.func1()
-						F:/Work/projects/git/auvitly/go-tools/recovery/builder.go:103 +0x433
-							panic({0x9141a0?, 0xb9f920?})
+					github.com/auvitly/go-tools/recovery.Builder.use.func1()
+						F:/Work/projects/git/auvitly/go-tools/recovery/builder.go:102 +0x345
+					panic({0xc031a0?, 0xe8e920?})
 						C:/Program Files/Go/src/runtime/panic.go:914 +0x21f
-							main.itsPanic({0x9cfcb0, 0xc00007a0e0}, {0x8fcc80, 0x9cd8b0})
+					main.itsPanic({0xcbecd0, 0xc00007a0e0}, {0xbebc80, 0xcbc8c0})
 						F:/Work/projects/git/auvitly/go-tools/examples/relax/main.go:36 +0x2
-							github.com/auvitly/go-tools/recovery.Builder.use({{0xc08360, 0x0, 0x0},{0xc08360, 0x0, 0x0}, 0x0, 0xc000044058, {0x965fa7, 0x2a}}, ...)
-						F:/Work/projects/git/auvitly/go-tools/recovery/builder.go:123 +0xee
-`							github.com/auvitly/go-tools/recovery.Builder.handle.func3()
-						F:/Work/projects/git/auvitly/go-tools/recovery/builder.go:219 +0x11f
-							created by github.com/auvitly/go-tools/recovery.Builder.handle in goroutine 1
-						F:/Work/projects/git/auvitly/go-tools/recovery/builder.go:215 +0x63d\n"
+					github.com/auvitly/go-tools/recovery.Builder.use({{0xef7360, 0x0, 0x0}, {0xef7360, 0x0, 0x0}, 0x0, 0xc000044058, {0xc54fcc, 0x2a}}, ...)
+						F:/Work/projects/git/auvitly/go-tools/recovery/builder.go:122 +0x87
+					github.com/auvitly/go-tools/recovery.Builder.handle.func3()
+						F:/Work/projects/git/auvitly/go-tools/recovery/builder.go:218 +0x11f
+					created by github.com/auvitly/go-tools/recovery.Builder.handle in goroutine 1
+						F:/Work/projects/git/auvitly/go-tools/recovery/builder.go:214 +0x63d
+				"
 			}
+		}
+		{
+			"code": "unknown",
+			"message": "Successfully assigned the error! Wow!"
 		}
 	]
 }
