e346b66d3399aa630b0ba19d8d86f83fd73f9130
Auvitly <nosfosters@mail.ru>
2024-06-09 22:35:41 +0300

    upd

--- nuclear/function/patch_amd64.go
+++ nuclear/function/patch_amd64.go
@@ -17,15 +17,15 @@ func moveDX(to uintptr) []byte {
 		byte(to >> 32),
 		byte(to >> 40),
 		byte(to >> 48),
-		byte(to >> 56), // movabs rdx, to
+		byte(to >> 56),
 	}
 }
 
 // jumpFar: JMP [RIP]
-// OpCode: 0xFF, 0x25, 0x00, 0x00, 0x00, 0x00
+// OpCode: 0xFF, 0x25, 0x00, 0x00, 0x00, 0x00 // jmp QWORD PTR
 func jumpFar(to uintptr) []byte {
 	return []byte{
-		0xFF, 0x25, 0x00, 0x00, 0x00, 0x00, // ff 25 00 00 00 00   jmp    QWORD PTR
+		0xFF, 0x25, 0x00, 0x00, 0x00, 0x00,
 		byte(to),
 		byte(to >> 8),
 		byte(to >> 16),
