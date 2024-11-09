27bc4ddb081bed4108dc4dff769fee7adffac247
Auvitly <nosfosters@mail.ru>
2024-11-09 21:57:29 +0300

    update lab package

--- standard/lab/test_test.go
+++ standard/lab/test_test.go
@@ -4,21 +4,48 @@ import (
 	"github.com/auvitly/go-tools/lab"
 	"github.com/auvitly/go-tools/lab/vault"
 	"github.com/stretchr/testify/require"
+	"io"
 	"net"
 	"testing"
+	"time"
 )
 
-func TestStoreLoad(t *testing.T) {
+func TestVault(t *testing.T) {
 	t.Parallel()
 
 	var tests = []lab.Test[
-		net.HardwareAddr,
-		net.HardwareAddr,
+		lab.TODO,
+		lab.TODO,
 	]{
 		{
-			Name: "#1:Storage:EqualValues",
+			Name: "#1:Vault:EqualValues:net.HardwareAddr",
 			In:   vault.Store(t, "mac", lab.Value(net.ParseMAC("b1:b2:1e:68:ab:d4"))),
 			Out:  vault.Load[net.HardwareAddr](t, "mac"),
+		},
+		{
+			Name: "#2:Vault:EqualValues:net.IP",
+			In:   vault.Store(t, "ip", lab.FirstValue(net.ParseCIDR("127.0.0.1/24"))),
+			Out:  vault.Load[net.IP](t, "ip"),
+		},
+		{
+			Name: "#3:Vault:EqualValues:*net.IPNet",
+			In:   vault.Store(t, "cidr", lab.SecondValue(net.ParseCIDR("127.0.0.1/24"))),
+			Out:  vault.Load[*net.IPNet](t, "cidr"),
+		},
+		{
+			Name: "#4:Vault:EqualValues:time.Time",
+			In:   vault.Store(t, "time", lab.Value(time.Parse(time.RFC3339, "2006-01-02T15:04:05+07:00"))),
+			Out:  vault.Load[time.Time](t, "time"),
+		},
+		{
+			Name: "#5:Vault:EqualValues:lab.Now",
+			In:   vault.Store(t, "now", lab.Now),
+			Out:  vault.Load[time.Time](t, "now"),
+		},
+		{
+			Name: "#6:Vault:EqualValues:error",
+			In:   vault.Store(t, "error", io.ErrClosedPipe),
+			Out:  vault.Load[error](t, "error"),
 		},
 	}
 
--- standard/lab/vault/vault.go
+++ standard/lab/vault/vault.go
@@ -13,8 +13,17 @@ var (
 	vault = make(map[*testing.T]map[string]any)
 )
 
+// Store - stores the specified value within the active pointer to the testing object *testing.T.
+// After the test is completed, the data will be deleted.
 func Store[V any](t *testing.T, key string, value V) (result V) {
 	t.Helper()
+
+	t.Cleanup(func() {
+		mu.Lock()
+		defer mu.Unlock()
+
+		delete(vault, t)
+	})
 
 	mu.Lock()
 	defer mu.Unlock()
@@ -43,6 +52,7 @@ func Store[V any](t *testing.T, key string, value V) (result V) {
 	return value
 }
 
+// Load - loading an object of type V from the testing object storage *testing.T.
 func Load[V any](t *testing.T, key string) (value V) {
 	t.Helper()
 
@@ -59,29 +69,4 @@ func Load[V any](t *testing.T, key string) (value V) {
 	}
 
 	return stored
-}
-
-func Copy[V any](t *testing.T, key string) (value V) {
-	t.Helper()
-
-	mu.Lock()
-	defer mu.Unlock()
-
-	if vault[t] == nil || vault[t][key] == nil {
-		t.Fatalf("not found value with key '%s'", key)
-	}
-
-	stored, ok := vault[t][key].(V)
-	if !ok {
-		t.Fatalf("stored value with key '%s' error: expected type %T, actual type %T", key, value, stored)
-	}
-
-	var (
-		src = reflect.ValueOf(stored)
-		dst = reflect.New(src.Type()).Elem()
-	)
-
-	reflect.Copy(dst, src)
-
-	return dst.Interface().(V)
 }
