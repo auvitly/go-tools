c9968ba6d277eb9304f0a7b7721b91f371b90b06
auvitly <nosfosters@mail.ru>
2025-04-06 21:53:03 +0300

    update sdk

--- standard/sdk/scheduler/inmemory/task.go
+++ standard/sdk/scheduler/inmemory/task.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/auvitly/go-tools/collection/stderrs"
 	"github.com/auvitly/go-tools/standard/sdk/scheduler/entity/task"
+	"github.com/auvitly/go-tools/standard/utils/reflector"
 	"github.com/google/uuid"
 )
 
@@ -21,5 +22,13 @@ func NewTaskStorage[T task.IsTask]() *TaskStorage[T] {
 }
 
 func (s *TaskStorage[T]) Get(ctx context.Context, id uuid.UUID) (T, *stderrs.Error) {
+	s.mu.RLock()
+	defer s.mu.Unlock()
 
+	record, ok := s.storage[id]
+	if !ok {
+		return reflector.Nil[T](), stderrs.NotFound.SetMessage("not found task with id '%s'", id.String())
+	}
+
+	return record, nil
 }
--- standard/utils/reflector/genetics_test.go
+++ standard/utils/reflector/genetics_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/auvitly/go-tools/standard/utils/reflector"
 	"github.com/auvitly/go-tools/test/lab"
+	"github.com/auvitly/go-tools/test/lab/vault"
 	"github.com/stretchr/testify/require"
 )
 
@@ -42,5 +43,52 @@ func TestNil(t *testing.T) {
 			require.Equal(t, test.In, test.Out)
 		})
 	}
+}
 
+func TestCopy(t *testing.T) {
+	var tests = []lab.Test[lab.Any, lab.Any]{
+		{
+			Name: "int",
+			In:   vault.Store(t, "int", 100),
+			Out: func() any {
+				var cp = reflector.Copy(vault.Load[int](t, "int"))
+
+				cp++
+
+				return cp
+			}(),
+		},
+		{
+			Name: "*int",
+			In:   vault.Store(t, "*int", lab.Pointer(100)),
+			Out: func() any {
+				var cp = reflector.Copy(vault.Load[*int](t, "*int"))
+
+				*cp++
+
+				return cp
+			}(),
+		},
+		{
+			Name: "map[string]string",
+			In: vault.Store(t, "map[string]string", map[string]string{
+				"key": "value",
+			}),
+			Out: func() any {
+				var cp = reflector.Copy(vault.Load[map[string]string](t, "map[string]string"))
+
+				cp["key"] = "another_value"
+
+				return cp
+			}(),
+		},
+	}
+
+	for i := range tests {
+		var test = tests[i]
+
+		t.Run(test.Name, func(t *testing.T) {
+			require.Equal(t, test.In, test.Out)
+		})
+	}
 }
